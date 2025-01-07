6fef8e265ff6de29726c9c17d3cf53f3f2b8a496
Cedric Louyot <cedric.louyot@gmail.com>
2025-01-07 04:57:45 +0100

    throw ParsingError from checkGpx

--- services/index.js
+++ services/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const checkGpx = require('./checkGpx');
+const compareTrack = require('./compareTrack');
 const gdrive = require('./gdrive');
 const mailer = require('./mailer');
 
 module.exports = {
+  compareTrack,
   checkGpx,
   gdrive,
   mailer,
